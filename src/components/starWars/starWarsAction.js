426ad23026555960df3f9255552fd716db0ea7af
Ninette <ninette.adhikari@gmail.com>
2020-01-21 20:29:33 +0100

    setup store

---
+++ src/components/starWars/starWarsAction.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default getStarWarsData = ()=>dispatch => {
+  axios.get('https://swapi.co/api/people').then(response => 
+  
+  );
+};
---
+++ src/components/starWars/starWarsReducer.js
@@ -0,0 +1,3 @@
+const initialState={
+    starWarsData=[]
+}

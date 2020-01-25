4d12852a0b2fc4baad26a42484be48a50b198c05
Ninette <ninette.adhikari@gmail.com>
2020-01-25 15:53:54 +0100

    hide api key

--- src/components/weather/weatherAction.js
+++ src/components/weather/weatherAction.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 export const getWeatherData = (city) => dispatch => {
-    const API_KEY="c378c988a41c0d1e5eea9ef6e88ef6a6"
-    // const API_KEY = "e5ced403947117877134523bd0915d32\n"
-//   axios.get(`http://api.openweathermap.org/data/2.5/weather?q=berlin,de&appid=${API_KEY}&units=metric`).then(res =>
-  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},de&appid=${API_KEY}&units=metric`).then(res =>
+//   axios.get(`http://api.openweathermap.org/data/2.5/weather?q=berlin,de&appid=${process.env.REACT_APP_API_KEY}&units=metric`).then(res =>
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},de&appid=${process.env.REACT_APP_API_KEY}&units=metric`).then(res =>
     dispatch({
       type: "GET_WEATHER",
       payload: res.data
